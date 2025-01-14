b52e0186588820fd0cf58b7da86417c93667a26d
Ankit Renduchintala <rankit92@gmail.com>
2025-01-13 23:12:05 -0500

    adding min in sorted rotated array

---
+++ src/FindMinRotatedSortedArray153.java
@@ -0,0 +1,22 @@
+public class FindMinRotatedSortedArray153 {
+
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
+        
+        while (left < right) {
+            int mid = left + (right-left)/2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+
+        return nums[left];
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
