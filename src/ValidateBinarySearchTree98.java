58f34ec3b6aa9807aa329e6f762ac6aa3fa526c6
Ankit Renduchintala <rankit92@gmail.com>
2025-03-11 00:01:07 -0400

    validate BST

---
+++ src/ValidateBinarySearchTree98.java
@@ -0,0 +1,18 @@
+public class ValidateBinarySearchTree98 {
+
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    boolean dfs(TreeNode root, long lowBound, long highBound) {
+        if (root == null) return true;
+
+        if (!(lowBound < root.val && root.val < highBound)) return false;
+
+        return dfs(root.left, lowBound, root.val) && dfs(root.right, root.val, highBound);
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
