c82b2fe01f72b461fca98921c13f861896144c04
Ankit Renduchintala <rankit92@gmail.com>
2025-03-25 00:48:38 -0400

    adding 2D DP sol

---
+++ src/Triangle120.java
@@ -0,0 +1,20 @@
+
+import java.util.List;
+
+public class Triangle120 {
+
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.size() + 1];
+
+        for (int i = triangle.size() - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
